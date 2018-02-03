97dc3732b67a7b6a16ee00a10fc29c3728471279
Aidan Calverley <14ac104@queensu.ca>
2018-02-03 19:24:29 +0000

    testing html

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //use express
 var express = require("express");
 var passport = require('passport');
+var bodyParser = require("body-parser");
 var current_date = require("current-date");
 var FitbitApiClient = require("fitbit-node");
 var app = express();
-
 var date = current_date('date', '-');
+
+app.use(bodyParser.urlencoded({extended: true}));
     
 //all files to be rendered are ejs (no longer need .ejs at the end)
 app.set("view enginer", "ejs");
@@ -13,8 +15,9 @@ app.set("view enginer", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 var resPath = "activities/distance/date/";
-var timePeriod = "1m"; 
-var url = "";
+var timePeriod; 
+var url;
+var accessToken;
 
 const client = new FitbitApiClient({
 	clientId: "22CLZZ",
@@ -22,26 +25,33 @@ const client = new FitbitApiClient({
 	apiVersion: '1.2' // 1.2 is the default
 });
 
+app.post("/getTimePeriod", function(req, res) {
+   timePeriod = req.body.timePeriod;
+   url = "/" + resPath + date + "/" + timePeriod + ".json";
+   res.redirect("/getDistance");
+});
+
 // redirect the user to the Fitbit authorization page
 app.get("/authorize", (req, res) => {
 	// request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
-	res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://immense-shelf-22042.herokuapp.com/userdata', 'login'));
+	res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://immense-shelf-22042.herokuapp.com/callback', 'login'));
 });
 
 // handle the callback from the Fitbit authorization flow
-app.get("/userdata", (req, res) => {
+app.get("/callback", (req, res) => {
 	// exchange the authorization code we just received for an access token
-	client.getAccessToken(req.query.code, 'https://immense-shelf-22042.herokuapp.com/userdata').then(result => {
+	client.getAccessToken(req.query.code, 'https://immense-shelf-22042.herokuapp.com/callback').then(result => {
 		// use the access token to hfetch the user's profile information
-		url = "/" + resPath + date + "/" + timePeriod + ".json";
-		console.log(url);
-		
-		client.get(url, result.access_token).then(results => {
-			res.send(results[0]);
-		});
+		accessToken = result.access_token;
 	}).catch(res.send);
 });
 
+
+app.get("/getDistance", (res, req) => {
+    client.get(url, (result) => {
+        res.send(result[0]);
+    });
+});
 
 
 //function that runs when loading a page (get request)
