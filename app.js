270b39b037545ec759323e0fc83ea2f22028d8b5
Aidan Calverley <14ac104@queensu.ca>
2018-02-03 09:24:39 +0000

    testing fitbit api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //use express
 var express = require("express");
 var passport = require('passport');
-var fitbitStrat = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;
+var FitbitApiClient = require("fitbit-node");
 var app = express();
     
 //all files to be rendered are ejs (no longer need .ejs at the end)
@@ -10,20 +10,35 @@ app.set("view enginer", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 
-passport.use(new fitbitStrat({
-    clientID: "22CLZZ",
-    clientSecret: "d392657b7f9473d6a77da8f99dfcbf7d",
-    callbackURL: "https://immense-shelf-22042.herokuapp.com/"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      
-      console.log(profile.id);
-      
-    // User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
-  }
-));
+
+
+const client = new FitbitApiClient({
+	clientId: "22CLZZ",
+	clientSecret: "d392657b7f9473d6a77da8f99dfcbf7d",
+	apiVersion: '1.2' // 1.2 is the default
+});
+
+// redirect the user to the Fitbit authorization page
+app.get("/authorize", (req, res) => {
+	// request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
+	res.redirect(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'https://immense-shelf-22042.herokuapp.com/', 'login'));
+});
+
+// handle the callback from the Fitbit authorization flow
+app.get("/callback", (req, res) => {
+	// exchange the authorization code we just received for an access token
+	client.getAccessToken(req.query.code, 'https://immense-shelf-22042.herokuapp.com/').then(result => {
+		// use the access token to fetch the user's profile information
+		client.get("/profile.json", result.access_token).then(results => {
+		    console.log("success!");
+			res.send(results[0]);
+		});
+	}).catch(res.send);
+});
+
+
+
+
 
 
 //function that runs when loading a page (get request)
