e789e47f4fa9ecf7a30552a4048a1a0558a6ae3e
Aidan Calverley <14ac104@queensu.ca>
2018-02-03 08:50:56 +0000

    testing fitbit api

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 //use express
-var express = require("express"),
-    app = express();
+var express = require("express");
+var passport = require('passport');
+var fitbitStrat = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;
+var app = express();
     
 //all files to be rendered are ejs (no longer need .ejs at the end)
 app.set("view enginer", "ejs");
 
 app.use(express.static(__dirname + '/public'));
+
+
+passport.use(new fitbitStrat({
+    clientID: "22CLZZ",
+    clientSecret: "d392657b7f9473d6a77da8f99dfcbf7d",
+    callbackURL: "https://immense-shelf-22042.herokuapp.com/"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      
+      console.log(profile.id);
+      
+    // User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+  }
+));
+
 
 //function that runs when loading a page (get request)
 app.get("/", function(req,res){
