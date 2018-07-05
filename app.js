13df308740cddcc0eff121ec9b31660460b9f61f
Chris Maltais <chrismaltais@Chriss-MacBook-Pro-2.local>
2018-07-04 20:39:53 -0400

    Add environment vars, format code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //use express
+require('dotenv').config();
 var express = require("express");
 var passport = require('passport');
 var current_date = require("current-date");
@@ -8,21 +9,22 @@ var getNearestCity = require('./public/JS/run-radius-geocoder.js');
 var formatQuery = require('./public/JS/formatQuery.js');
 var app = express();
 const port = process.env.PORT || 3000;
-const api_key = process.env.FITBIT_API_KEY; // config done in heroku
+const api_key = process.env.geolocation_api_key; // config done in heroku
 const callbackURL = "http://www.wherecouldihavegone.com/callback";
+const devCallbackURL = "http://localhost:3000/callback";
 
 
 app.use(express.static(__dirname + '/public'));
 
 var resPath = "activities/distance/date/";
-var timePeriod; 
+var timePeriod;
 var url;
 var accessToken;
 var userLat, userLong;
 
 const client = new FitbitApiClient({
-  clientId: "22CLZZ",
-  clientSecret: "d392657b7f9473d6a77da8f99dfcbf7d",
+  clientId: process.env.fitbit_clientId,
+  clientSecret: process.env.fitbit_clientSecret,
   apiVersion: '1.2' // 1.2 is the default
 });
 
@@ -30,23 +32,24 @@ const client = new FitbitApiClient({
 app.get("/authorize", (req, res) => {
   console.log("calling fitbit api");
     // request access to the user's activity, heartrate, location, nutrion, profile, settings, sleep, social, and weight scopes
-  res.redirect(client.getAuthorizeUrl('activity heartrate location profile settings social weight', callbackURL, 'login'));
+  res.redirect(client.getAuthorizeUrl('activity heartrate location profile settings social weight', devCallbackURL, 'login'));
 });
 
 
 app.get('/callback', async function(req, res) {
-    const {access_token:accessToken} = await client.getAccessToken(req.query.code, callbackURL);
+    const {access_token:accessToken} = await client.getAccessToken(req.query.code, devCallbackURL);
     // console.log(access_token);
     const date = current_date('date', '-');
     const ugandaChildDistance = (3 * 30) * 12;
     const url = `/activities/distance/date/${date}/3m.json`;
-  
-    const [body, response] = await client.get(url, accessToken); 
+
+    const [body, response] = await client.get(url, accessToken);
     // console.log(response.statusCode, body["activities-distance"]);
 
     const distanceSum = body["activities-distance"].reduce((sum, {value})=>sum + Number(value), 0).toFixed(2);
 
     console.log("distance sum: ", distanceSum);
+    console.log(api_key);
 
     const geolocationResponse = JSON.parse(await request.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${api_key}`));
 
@@ -55,7 +58,7 @@ app.get('/callback', async function(req, res) {
     userLat = geolocationResponse.location.lat;
     userLong = geolocationResponse.location.lng;
     // res.send(`lat is: ${userLat}, long is: ${userLong}`);
-    
+
     var user_nearestCity = await getNearestCity(userLat, userLong, distanceSum);
 
     var ugandaChild_nearestCity = await getNearestCity(userLat, userLong, ugandaChildDistance);
@@ -74,11 +77,18 @@ app.get('/callback', async function(req, res) {
 
     // console.log("destination:", destination);
     // console.log("origin: ", origin);
-    
-    res.render("maps_2path.ejs", {distanceTraveled: distanceSum, userLat: userLat, userLong: userLong, user_destination: user_destination, ugandaChild_destination: ugandaChild_destination, origin: origin});
+
+    res.render("maps_2path.ejs", {
+    	distanceTraveled: distanceSum,
+      	userLat: userLat,
+      	userLong: userLong,
+      	user_destination: user_destination,
+      	ugandaChild_destination: ugandaChild_destination,
+      	origin: origin
+    });
     // res.render("map-test.ejs");
 });
-    
+
 
 app.get("/getDistance", (req, res) => {
     client.get(url, accessToken).then(result => {
@@ -88,13 +98,19 @@ app.get("/getDistance", (req, res) => {
 
 //function that runs when loading a page (get request)
 app.get("/test", function(req, res){
-    res.render("maps_1path.ejs", {distanceTraveled: 100, userLat: 50, userLong: -50, destination: "372 Maple St, Deseronto, ON K0K 1X0, Canada", origin: "119 Collingwood St, Kingston, ON K7L 3X6, Canada"});
+    res.render("maps_1path.ejs", {
+        distanceTraveled: 100,
+        userLat: 50,
+        userLong: -50,
+        destination: "372 Maple St, Deseronto, ON K0K 1X0, Canada",
+        origin: "119 Collingwood St, Kingston, ON K7L 3X6, Canada"
+    });
 });
 
 app.get("*", function(req, res){
-   res.send("Sorry, page not found"); 
+   res.send("Sorry, page not found");
 });
 
 app.listen(port, process.env.IP, function(){
-    console.log("Server has started!");
+    console.log(`Server has started! Running on port: ${port}`);
 });
